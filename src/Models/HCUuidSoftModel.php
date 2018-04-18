add435e1328edb89236853a68005563d2fc46b6a
Mantas Paulauskas <mantopastas@gmail.com>
2018-04-18 13:27:45 +0300

    add new uuid soft model and remove softdeletes trait from huuid model

--- src/Models/HCUuidSoftModel.php
+++ src/Models/HCUuidSoftModel.php
@@ -29,41 +29,13 @@ declare(strict_types = 1);
 
 namespace HoneyComb\Starter\Models;
 
-use Ramsey\Uuid\Uuid;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
- * Class HCUuidModel
+ * Class HCUuidSoftModel
  * @package HoneyComb\Starter\Models
  */
-class HCUuidModel extends HCModel
+class HCUuidSoftModel extends HCUuidModel
 {
-    /**
-     * Soft delete database field.
-     *
-     * @var array
-     */
-    protected $dates = ['deleted_at'];
-
-    /**
-     * The "booting" method of the model.
-     *
-     * @return void
-     */
-    protected static function boot(): void
-    {
-        parent::boot();
-
-        /**
-         * Attach to the 'creating' Model Event to provide a UUID
-         * for the `id` field (provided by $model->getKeyName())
-         */
-        static::creating(function ($model) {
-            if (!isset($model->attributes['id'])) {
-                $model->attributes['id'] = Uuid::uuid4()->toString();
-            }
-
-            /** @var HCUuidModel $model */
-            $model->{$model->getKeyName()} = $model->attributes['id'];
-        });
-    }
+    use SoftDeletes;
 }
