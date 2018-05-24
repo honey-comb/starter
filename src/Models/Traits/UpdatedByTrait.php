ea299d829e7e2f334dc9616b771ea0539a1cfa7a
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2018-05-24 12:12:31 +0300

    Added createdBy and updatedBy traits for models

---
+++ src/Models/Traits/UpdatedByTrait.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @copyright 2018 innovationbase
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Contact InteractiveSolutions:
+ * E-mail: hello@innovationbase.eu
+ * https://www.innovationbase.eu
+ */
+
+namespace HoneyComb\Starter\Models\Traits;
+
+use HoneyComb\Core\Models\HCUser;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+trait UpdatedByTrait
+{
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot(): void
+    {
+        parent::boot();
+
+        /**
+         * Attach to the 'creating' Model Event to provide record with user id which is now creating this record
+         */
+        static::updating(function($model) {
+
+            if (auth()->user()) {
+                $updatorId = auth()->id();
+            } else {
+                $updatorId = null;
+            }
+
+            $model->{'updated_by'} = $updatorId;
+        });
+    }
+
+    /**
+     * Returning updator
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function updator(): HasOne
+    {
+        return $this->hasOne(HCUser::class, 'id', 'updated_by');
+    }
+}
